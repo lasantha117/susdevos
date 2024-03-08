749ec5004d33a8259a3204f94377bf51ab3c1c9a
paranga <paranga@gmail.com>
2024-03-08 15:15:47 +0530

    Add three images public/Images/Case/img1.jpg/im2.jpg/img3.jpg and update CasePage Components

--- components/Case/CasePage.tsx
+++ components/Case/CasePage.tsx
@@ -3,32 +3,27 @@ import React from "react";
 
 const products = [
   {
-    id: 1,
-    name: 'Development of a Industrial park',
-    button: '#',
-    href: '#',
-    imageSrc:
-      'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT34IawD3nXe7WNvnAEr1oiyI-KZOn6zpW7-UZgmvERDSoTYEjM',
+    //Picture  01
+    id: 1, //Picture ID
+    name: 'Development of a Industrial park', //Picture Description 
+    href: '#', //Read more Url
+    imageSrc: '/Images/Case/img1.jpg', //Image Path
     imageAlt: '',
   },
 
   {
-    id: 2,
-    name: 'Road extension through a city and forest area',
-    button: '#',
-    href: '#',
-    imageSrc:
-      'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSh_XjeoX__fgxUnBU-L3dLjxCfjMSwnZ-YF6EuifhzZFl0DuBb',
+    id: 2, //Picture ID
+    name: 'Road extension through a city and forest area', //Picture Description 
+    href: '#', //Read more Url
+    imageSrc: '/Images/Case/img2.jpg', //Image Path
     imageAlt: '',
   },
 
   {
-    id: 3,
-    name: '100MW Solar & 500MW Wind power plants',
-    button: '#',
-    href: '#',
-    imageSrc:
-      'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTrhDNY18bD5feY9VFd6aqlo1eGC3tE7ZCNBvcVb6ZuJPMYd1oi',
+    id: 3, //Picture ID
+    name: '100MW Solar & 500MW Wind power plants', //Picture Description
+    href: '#', //Read more Url
+    imageSrc: '/Images/Case/img3.jpg', //Image Path
     imageAlt: '',
   },
 ];
@@ -36,7 +31,7 @@ const products = [
 
 export default function CasePage(){
   return(
-    <div className = 'p-64 mx-auto bg-white bg-opacity-80 border border-black border-opacity-80  text-3xl text-center '>
+    <div className = 'p-64 mx-16  border-opacity-80  text-3xl text-center '>
       <div className = 'bg-white mt-[10px]'>
        <h1 className = 'lg:text-[48px] md:text-xl sm:text-6xl text-black text-center font-medium font-Outfit tracking-tight '> Case Studies </h1> 
        <p className = 'lg:text-[24px] md:text-xl sm:text-6xl  text-black text-center font-medium font-Outfit leading-8  mt-16 '> Exploring Real-World Success Stories: How SusDev-OS Drives Environmental Impact and Sustainability </p>
@@ -63,9 +58,9 @@ export default function CasePage(){
                   </h3> 
                 </div>
                 
+                {/* Button */}
                 <div>
-                  <button
-                    className=" bg-white flex-none  mt-6 block  px-3 py-2  text-sm shadow-sm hover:bg-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 rounded-[100px] border-[2px] border-black text-black font-semibold lg:w-[164px] lg:h-[44px] text-center lg:text-[20px] font-Outfit ">
+                  <button className=" hover:bg-black bg-white flex-none  mt-6 block  px-3 py-2  text-sm shadow-sm hover:bg-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 rounded-[100px] border-[2px] border-black text-black font-semibold lg:w-[164px] lg:h-[44px] text-center lg:text-[20px] font-Outfit ">
                     Read More 
                   </button>
                 </div>
@@ -77,4 +72,3 @@ export default function CasePage(){
       </div>   
   );
 }
-
