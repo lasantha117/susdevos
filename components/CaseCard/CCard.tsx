fff789a515161cbc620c6ef473a0e038410fbdfc
paranga <paranga@gmail.com>
2024-03-11 11:07:21 +0530

    Add comments

--- components/CaseCard/CCard.tsx
+++ components/CaseCard/CCard.tsx
@@ -21,20 +21,26 @@ const Card: React.FC<CardProps> = ({ cardData }) => {
           className={`sm:px-4 sm:py-2 min-w-48 max-w-52 min-h-52 sm:max-w-64 sm:max-h-fit md:m-w-64 md:max-h-fit rounded-2xl lg:px-6 md:py-4  lg:min-w-96 lg:max-h-fit lg:rounded-3xl border-solid mt-2 ${cardCn}`}>
           
           <img
-            className="sm:w-14 sm:h-32 lg:w-[550px] lg:h-[384px] ml-4 lg:ml-6 lg:mt-6"
+            className="sm:w-64 sm:h-64 lg:w-[550px] lg:h-[384px] sm:ml-2 sm:mt-2 lg:ml-6 lg:mt-6"
             src={imgSrc}
             alt="logo"
           />
           
           <p
-            className={`mx-4 mt-4 text-xs lg:mx-6 lg:mt-8 font-medium lg:text-2xl lg:mb-5 ${pCn}`}
+            className={`mx-4 mt-4 text-xs lg:mx-6 lg:mt-8 font-medium lg:text-2xl lg:mb-5 font-s-[30px] font-family:"outfit" ${pCn}`}
           >
             {content}
           </p>
 
           <Button
-            className={`bg-white flex-none w-32 h-10 mt-6 block rounded-md px-3 py-2 text-center text-sm font-semibold text-black shadow-sm hover:bg-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${buttonCn}`}>
-            Read More
+            
+            className={`bg-white flex-none lg:w-[164px] lg:h-[44px] lg:gap-[10px] lg:mt-6 justify-center  block rounded-md sm:px-2 sm:py-2 sm:mt-4 lg:px-6 lg:py-2  text-center text-sm font-semibold text-black shadow-sm hover:bg-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${buttonCn}`}>
+ 
+            Read More <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+</svg>
+            
+
           </Button>
         </div>
       </div>
--- components/CaseCard/Index.tsx
+++ components/CaseCard/Index.tsx
@@ -10,34 +10,34 @@ const Case: React.FC = () => {
   const cardDataArray = [
     {
       imgSrc: '/Images/CaseCard/img1.jpg',
-      content: 'Development of a Industrial park',
-      cardCn: '',
-      pCn: 'text-slate-950',
-      buttonCn: 'text-black bg-white bg-slate-950',
+      content: 'Development of a Industrial park', //Discription of the case study
+      cardCn: '', //Card class name
+      pCn: 'text-slate-950 ', //Discription (pCn: paragraph class name)
+      buttonCn: 'text-black bg-white bg-slate-950', //Button  class name
     },
     {
       imgSrc: '/Images/CaseCard/img2.jpg',
-      content: 'Road extension through a city and forest area',
-      cardCn: '',
-      pCn: 'text-slate-950',
-      buttonCn: 'text-black bg-white bg-slate-950',
+      content: 'Road extension through a city and forest area', //Discription of the case study
+      cardCn: '', //Card class name
+      pCn: 'text-slate-950', //Discription (pCn: paragraph class name)
+      buttonCn: 'text-black bg-white bg-slate-950', //Button  class name
     },
     {
       imgSrc: '/Images/CaseCard/img3.jpg',
-      content: '100MW Solar & 500MW Wind power plants',
-      cardCn: '',
-      pCn: 'text-slate-950',
-      buttonCn: 'text-black bg-white bg-slate-950',
+      content: '100MW Solar & 500MW Wind power plants', //Discription of the case study
+      cardCn: '', //Card class name
+      pCn: 'text-slate-950', //Discription (pCn: paragraph class name)
+      buttonCn: 'text-black bg-white bg-slate-950', //Button  class name
     },
   ];
 
   return (
     <>
       <div className="flex-none mx-auto justify-center mt-24">
-        <h1 className="flex-none text-2xl lg:text-5xl font-semibold text-center">
+        <h1 className="flex-none text-2xl lg:text-5xl font-s-[48px] text-center  font-bold font-family:outfit">
       Case Studies 
         </h1>
-        <p className="font-medium mb-10 mx-6 px-6 mt-6 md:px-8 lg:px-10 md:mt-8 lg:mt-10 md:mx-24 lg:mx-32 xl:mx-56 text-center lg:text-2xl">
+        <p className="font-medium mb-10 mx-6 px-6 mt-6 md:px-8 lg:px-10 md:mt-8 lg:mt-10 md:mx-24 lg:mx-32 xl:mx-56 text-center lg:text-2xl font-s-[24px] font-family:outfit">
      Exploring Real-World Success Stories: How SusDev-OS Drives Environmental Impact and Sustainability
         </p>
 
