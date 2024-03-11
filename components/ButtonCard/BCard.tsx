c4d0c70e1e16b21dcf6d24e998abc7940c3a2791
iamarasekera <iamarasekera.dev@gmail.com>
2024-03-11 20:33:41 +0800

    Updated image urls and changed Image tag to Img

--- components/ButtonCard/BCard.tsx
+++ components/ButtonCard/BCard.tsx
@@ -14,12 +14,15 @@ const Card: React.FC<CardProps> = ({ title, content,image,button }) => {
     //Adding top image in the card
     <div className="rounded-3xl /*border-4 border-black*/ border-solid  p-4 h-full  bg-gray-50">  
       <div className="relative rounded-full overflow-hidden h-20 w-20 bg-[#2AD2C9] bg-opacity-64">
-        <Image 
-          src={image} 
-          alt={title} 
-          layout="fill" 
-          objectFit="cover" 
-          className="rounded-t-xl p-4" 
+      <img
+          src={image}
+          alt={title}
+          className="rounded-t-xl p-4"
+          style={{
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%'
+          }}
         />
       </div>
       {/*Adding Title and contaent of the card */}
@@ -30,14 +33,17 @@ const Card: React.FC<CardProps> = ({ title, content,image,button }) => {
       {/*Adding below button*/}
       <div className="flex justify-end items-end">
           <div  className=" h-12 w-12  flex items-center justify-center rounded-full">
-              <Image 
-                src="/Images/ButtonCard/CircledUpRight.webp" 
-                alt="Button Image"
-                width={32}
-                height={32}
-                className="rounded-full hover:bg-black" 
-                layout="responsive"
-              />
+          <img
+            src="/Images/ButtonCard/CircledUpRight.png"
+            alt="Button Image"
+            width={32}
+            height={32}
+            className="rounded-full hover:bg-black"
+            style={{
+              width: '100%',
+              height: 'auto'
+            }}
+          />
               {button}
           </div>
       </div>
