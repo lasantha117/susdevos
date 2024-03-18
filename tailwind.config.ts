e0949c18cc079a9aa13a0814cb4d384182ede4c7
paranga <paranga@gmail.com>
2024-03-18 12:49:52 +0530

    Remove spacing from config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,10 +20,6 @@ const config: Config = {
         text_brand_secondary:'#9FE870',
         // Add more colors as needed
       },
-
-    spacing: {
-      vw10: "10vw",
-    }
     },
   },
   plugins: [],
