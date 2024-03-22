43522c663dfce88d203b8266fb4e9420987e940b
yohan9878 <it20256050@my.sliit.lk>
2024-03-22 09:31:44 +0530

    Fixed Responsiveness About card Component and changed image tags to Next Image tag

--- components/AboutCard/index.tsx
+++ components/AboutCard/index.tsx
@@ -14,7 +14,7 @@ const About: React.FC = () => {
         'How SusDev OS resolves typical issues facing climate impact mitigation action',
       cardCn: 'shadow-lg bg-teal-400 ',
       pCn: 'text-slate-950',
-      buttonCn: 'text-white bg-slate-950',
+      buttonCn: 'text-white bg-slate-950 hover:bg-white hover:text-slate-950',
     },
     {
       imgSrc: '/Images/AboutCard/image02.webp',
@@ -22,7 +22,7 @@ const About: React.FC = () => {
         'Are you from the private sector looking to de-carbonize? We have you covered',
       cardCn: 'bg-slate-950 shadow-lg',
       pCn: 'text-gray-100',
-      buttonCn: 'bg-white text-black',
+      buttonCn: 'bg-white text-slate-950 hover:bg-slate-500 hover:text-white',
     },
     {
       imgSrc: '/Images/AboutCard/image03.webp',
@@ -30,13 +30,13 @@ const About: React.FC = () => {
         'AI-powered insights for UN-FCCC, NDC, UN-SDG, SBTi, Sendai framework compliance',
       cardCn: 'bg-orange-300 shadow-lg',
       pCn: 'text-slate-950',
-      buttonCn: 'text-white bg-slate-950',
+      buttonCn: 'text-white bg-slate-950 hover:bg-white hover:text-slate-950',
     },
   ];
 
   return (
     <>
-      <div className="flex-none mx-auto justify-center mt-24">
+      <div className="flex-none mx-auto justify-center bg-white py-20">
         <h1 className="flex-none text-2xl lg:text-5xl font-semibold text-center">
           Why SusDev OS
         </h1>
@@ -50,7 +50,7 @@ const About: React.FC = () => {
           automation tools needed for climate-smart development.
         </p>
 
-        <div className="grid grid-cols-1 2xl:grid-cols-3 mx-24 gap-6">
+        <div className="grid grid-cols-1 xl:grid-cols-3 mx-24 gap-6  mb-20">
         {cardDataArray.map((cardData, index) => (
           <Card key={index} cardData={cardData} />
         ))}
