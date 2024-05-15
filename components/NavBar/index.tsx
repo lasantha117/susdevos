32c9d8af59dec3d64536f13d6de1365649613dda
yohan9878 <it20256050@my.sliit.lk>
2024-05-15 11:56:50 +0530

    Modified default html component to react-aria-components

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 // Importing necessary modules
 import React, { useState } from 'react';
+import { Button } from 'react-aria-components';
 
 // The NavBar Component
 const NavBar = () => {
@@ -27,7 +28,7 @@ const NavBar = () => {
           </div>
           <div className="-mr-2 flex xl:hidden">
             {/* Button for toggling menu */}
-            <button onClick={toggleMenu} className="inline-flex items-center justify-center p-2 rounded-md text-teal-400 hover:text-white hover:bg-teal-700 focus:outline-none focus:bg-teal-700 focus:text-white">
+            <Button onPress={toggleMenu} className="inline-flex items-center justify-center p-2 rounded-md text-teal-400 hover:text-white hover:bg-teal-700 focus:outline-none focus:bg-teal-700 focus:text-white">
               {/* SVG icon for closed menu */}
               <svg className={`${isOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -36,7 +37,7 @@ const NavBar = () => {
               <svg className={`${isOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
-            </button>
+            </Button>
           </div>
           {/* Container for buttons and email link on web view */}
           <div className="hidden xl:block absolute right-0 mr-4 xl:mr-24">
@@ -48,8 +49,8 @@ const NavBar = () => {
               </h2>
               {/* Grid container for buttons */}
               <div className="grid grid-cols-2 gap-2">
-                <button className="h-12 w-32 border-solid border-1 outline outline-2 outline-slate-900 text-slate-900 rounded-lg bg-white hover:bg-slate-200">Sign In</button>
-                <button className="h-12 w-32 border-solid border-1 rounded-lg bg-slate-900 text-white hover:bg-slate-800">Register</button>
+                <Button className="h-12 w-32 border-solid border-1 outline outline-2 outline-slate-900 text-slate-900 rounded-lg bg-white hover:bg-slate-200">Sign In</Button>
+                <Button className="h-12 w-32 border-solid border-1 rounded-lg bg-slate-900 text-white hover:bg-slate-800">Register</Button>
               </div>
             </div>
           </div>
@@ -66,8 +67,8 @@ const NavBar = () => {
           </h2>
           {/* Grid container for buttons */}
           <div className="grid grid-cols-1 gap-2">
-            <button className="h-12 w-full border-solid border-1 outline outline-2 outline-slate-900 text-slate-900 rounded-lg bg-white hover:bg-slate-200">Sign In</button>
-            <button className="h-12 w-full border-solid border-1 rounded-lg bg-slate-900 text-white hover:bg-slate-800">Register</button>
+            <Button className="h-12 w-full border-solid border-1 outline outline-2 outline-slate-900 text-slate-900 rounded-lg bg-white hover:bg-slate-200">Sign In</Button>
+            <Button className="h-12 w-full border-solid border-1 rounded-lg bg-slate-900 text-white hover:bg-slate-800">Register</Button>
           </div>
         </div>
       </div>
