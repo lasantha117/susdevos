17c64105086a61c4c1b2edce637352614d0e02c8
yohan9878 <it20256050@my.sliit.lk>
2024-05-15 11:57:57 +0530

    Implement go back and take me home button navigations in 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { Button, Link } from "react-aria-components";
+
 export default function NotFound() {
   return (
     <div className="items-center justify-center p-24 h-screen w-screen text-center mx-auto place-content-center">
@@ -7,12 +10,12 @@ export default function NotFound() {
       </p>
 
       <div className="flex gap-2 justify-center">
-        <button className="font-medium border-2 bg-white p-2 rounded-lg text-slate-950 hover:bg-slate-100 w-[163px] h-[60px]">
+        <Button onPress={() => window.history.back()} className="font-medium border-2 bg-white p-2 rounded-lg text-slate-950 hover:bg-slate-100 w-[163px] h-[60px]">
           Go Back
-        </button>
-        <button className="font-medium border-2 bg-slate-950 text-white p-2 rounded-lg hover:bg-slate-800 w-[163px] h-[60px]">
-          Take me home
-        </button>
+        </Button>
+        <Button onPress={() => window.history.back()} className="font-medium border-2 bg-slate-950 text-white p-2 rounded-lg hover:bg-slate-800 w-[163px] h-[60px]">
+          <Link href="/login">Take me home</Link>
+        </Button>
       </div>
     </div>
   );
