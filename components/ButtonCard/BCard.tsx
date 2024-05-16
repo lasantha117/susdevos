05ab253a217281923360e42f7f0e534b400b9e96
Chathura-Nimsara <99wakcnk@gmail.com>
2024-05-16 19:13:38 +0530

    Fix the all the erros in susDev OS and Button Card and Non Button Card

--- components/ButtonCard/BCard.tsx
+++ components/ButtonCard/BCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import Image from 'next/image';
+import React from 'react';
 
 //Create main variable for creating card
 interface CardProps {
@@ -9,40 +9,42 @@ interface CardProps {
   buttonLogic: boolean;
 }
 
-const Card: React.FC<CardProps> = ({ title, content,image,buttonLogic }) => {
+const Card: React.FC<CardProps> = ({ title, content, image, buttonLogic }) => {
   return (
     //Adding top image in the card
-    <div className="rounded-3xl border-2 border-gray-200 border-solid  p-4 h-full  bg-gray-50"> 
-    <div className="relative rounded-full overflow-hidden h-20 w-20 bg-[#2AD2C9] bg-opacity-64 basis-1/6">
-      <Image
+    <div className="flex flex-col rounded-3xl border-2 border-gray-200 border-solid p-4 h-full bg-gray-50">
+      <div className="flex justify-start rounded-full w-20 h-20 bg-[#2AD2C9] bg-opacity-64">
+        <Image
           src={image}
           alt={title}
-          width={50}
-          height={50}
-          className="rounded-t-xl p-4"
-          style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+          width={100}
+          height={100}
+          className="p-4"
         />
       </div>
-      {/*Adding Title and contaent of the card */}
-      <div className="p-4 h-fit">
-        <h1 className="pt-6 text-xl font-bold mb-2 h-20">{title}</h1>
-        <p className="pt-6 text-slate-400 subpixel-antialiased mb-4 h-40">{content}</p>
+
+      {/*Adding Title and content of the card */}
+      <div className="p-4">
+        <h1 className="pt-6 text-xl font-bold mb-2 lg:min-h-36 xl:min-h-32 2xl:min-h-20">{title}</h1>
+        <p className="pt-6 text-slate-400 subpixel-antialiased mb-4">
+          {content}
+        </p>
       </div>
+
       {/*Adding below button*/}
-      <div className="flex justify-end">
-        {buttonLogic  && (<button  className=" h-12 w-12  flex items-center justify-center rounded-full">
-          <Image
-            src="/Images/ButtonCard/CircledUpRight.png"
-            alt="Button Image"
-            width={50}
-            height={50}
-            className="rounded-full hover:bg-slate-300"
-            style={{ objectFit: 'contain', width: '100%', height: '100%' }}
-          />
-          </button>)}
-          
+      <div className="flex justify-end items-end flex-grow">
+        {buttonLogic && (
+          <button className="h-12 w-12 flex items-center justify-center rounded-full">
+            <Image
+              src="/Images/ButtonCard/CircledUpRight.png"
+              alt="Button Image"
+              width={50}
+              height={50}
+              className="rounded-full hover:bg-slate-300"
+            />
+          </button>
+        )}
       </div>
-      
     </div>
   );
 };
--- components/SusDevOsMain/index.tsx
+++ components/SusDevOsMain/index.tsx
@@ -8,12 +8,12 @@ const SusDev: React.FC = () => {
       {/* Left Content */}
       <div className='w-full sm:w-1/3 md:w-1/2 h-full'>
         <div className='h-1/2'>
-          <h3 className='pt-6 text-center sm:text-left md:text-center sm:pl-28 text-xl font-bold mb-2 sm:h-20'>Realtime data from development projects</h3>
-          <p className='text-center sm:text-left md:text-center sm:pl-28'>Projects location, ESG, resource footprint data digitized.</p>
+          <h3 className='pt-6 text-center xl:text-left md:text-center sm:pl-28 text-xl font-bold mb-2 sm:h-20'>Realtime data from development projects</h3>
+          <p className='text-center xl:text-left md:text-center sm:pl-28'>Projects location, ESG, resource footprint data digitized.</p>
         </div>
         <div className='h-1/2'>
-          <h3 className='pt-6 text-center sm:text-left md:text-center sm:pl-28 text-xl font-bold mb-2 sm:h-20'>Tangible climate reporting</h3>
-          <p className='text-center sm:text-left md:text-center sm:pl-28'>Manage and streamline work towards tangible sustainable development.</p>
+          <h3 className='pt-6 text-center xl:text-left md:text-center sm:pl-28 text-xl font-bold mb-2 sm:h-20'>Tangible climate reporting</h3>
+          <p className='text-center xl:text-left md:text-center sm:pl-28'>Manage and streamline work towards tangible sustainable development.</p>
         </div>
       </div>
       {/* Middle Image  */}
@@ -29,12 +29,12 @@ const SusDev: React.FC = () => {
       {/* Right Content */}
       <div className='w-full sm:w-1/3 md:w-1/2 h-full sm:ml-2'>
       <div className='h-1/2'>
-          <h3 className='text-center sm:text-left md:text-center pt-6 sm:pr-28 text-xl font-bold mb-2 sm:h-20 '>Workflow lifecycle management</h3>
-          <p className='text-center sm:text-left md:text-center sm:pr-28'>Projects and their sub-projects workflow is automated across stakeholders.</p>
+          <h3 className='text-center xl:text-left md:text-center pt-6 sm:pr-28 text-xl font-bold mb-2 sm:h-20 '>Workflow lifecycle management</h3>
+          <p className='text-center xl:text-left md:text-center sm:pr-28'>Projects and their sub-projects workflow is automated across stakeholders.</p>
         </div>
         <div className='h-1/2'>
-          <h3 className='text-center sm:text-left md:text-center pt-6 sm:pr-28 text-xl font-bold mb-2 sm:h-20'>Collaborative, Node mesh & AI</h3>
-          <p className='text-center sm:text-left md:text-center sm:pr-28'>Data shared with SaaS/ self hosted nodes on demand. AI Q&A - suggestions support work. API integration with othe IS.</p>
+          <h3 className='text-center xl:text-left md:text-center pt-6 sm:pr-28 text-xl font-bold mb-2 sm:h-20'>Collaborative, Node mesh & AI</h3>
+          <p className='text-center xl:text-left md:text-center sm:pr-28'>Data shared with SaaS/ self hosted nodes on demand. AI Q&A - suggestions support work. API integration with othe IS.</p>
         </div>
       </div>
   </div>
